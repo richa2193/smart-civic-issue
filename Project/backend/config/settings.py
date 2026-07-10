39c57a7e078e6fa28b6a043dd538c247c31bae05
Richa Parmar <richaparmar0109@gmail.com>
2026-07-10 18:23:31 +0530

    requirement file

--- Project/backend/config/settings.py
+++ Project/backend/config/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure--w6@llg6wbv=ws3ct7l#k#-%6dc_n+f^5f2hz(lnel_)ctr#1w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
